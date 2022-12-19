2f84a85ec6fd9a637d910baa7c919c6acc15d6c1
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-12-19 17:02:44 +0700

    adding logout

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,8 @@ Route::get('/', [MessagesController::class, 'index']);
 Route::get('/login', [LoginController::class, 'index']);
 Route::post('/login', [LoginController::class, 'authenticate']);
 
+Route::post('/logout', [LoginController::class, 'logout']);
+
 Route::get('/register', [RegisterController::class, 'index']);
 Route::post('/register', [RegisterController::class, 'store']);
 
