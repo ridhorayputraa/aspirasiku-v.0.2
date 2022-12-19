0490f5a03c4eadc9ded92767ea774dbfe6d3f0bc
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-12-19 16:58:06 +0700

    editing navbar with auth and logout

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -6,11 +6,45 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
 
-        <ul class="navbar-nav ms-auto ">
-            <li class="nav-item">
-                <a class="nav-link active text-white" aria-current="page" href="#">Login</a>
-            </li>
-        </ul>
+
+
+
+
+         {{-- method dari middleware --}}
+
+         <ul class="navbar-nav ms-auto">
+            @auth
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Welcome back, {{ auth()->user()->name }}
+                  {{-- ambil field name --}}
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-sidebar-reverse"></i> My Dashboard</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li>
+                   <form action="/logout" method="post">
+                    @csrf
+                    <button type="submit" class="dropdown-item" href="#"><i class="bi bi-box-arrow-right"></i>  Logout </button>
+                   </form>
+
+                </ul>
+              </li>
+
+            @else
+            {{-- Jika belum login --}}
+
+
+                <li class="nav-item">
+                  <a href="/login" class="nav-link {{ ($active === 'login') ? 'active' : ''"><i class="bi bi-box-arrow-right"></i>Login</a>
+
+                </li>
+              </ul>
+
+            @endauth
+
+
       </div>
     </div>
   </nav>
