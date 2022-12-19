741794e16fc52bad87960ef1b6cbfc18bd79ea95
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-12-19 16:42:08 +0700

    editing views login for form and create new routes

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -19,14 +19,14 @@
 
         <main class="form-signin w-100 mt-5 m-auto">
             <h1 class="h3 mb-3 fw-normal text-center">Please login</h1>
-            <form>
+            <form action="/login" method="post">
               <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput">Email address</label>
+                <input type="email" class="form-control" value="{{ old('email') }}" name="email" required id="email" placeholder="name@example.com">
+                <label for="email">Email address</label>
               </div>
               <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                <label for="floatingPassword">Password</label>
+                <input type="password" required class="form-control" name="password" id="password" placeholder="Password">
+                <label for="password">Password</label>
               </div>
 
 
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [MessagesController::class, 'index']);
 
 Route::get('/login', [LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'authenticate']);
+
 Route::get('/register', [RegisterController::class, 'index']);
 Route::post('/register', [RegisterController::class, 'store']);
 
