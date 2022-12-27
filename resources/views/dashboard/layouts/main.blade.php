55a4031d048bcc414eb41408b5195b32d97f10cf
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-12-27 17:56:37 +0700

    adding icon

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="/assets/css/main/app.css">
     <link rel="stylesheet" href="/assets/css/main/app-dark.css">
 
+    <link rel="shortcut icon" class="rounded-circle" href="/img/aspirasiku.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
 <link rel="stylesheet" href="/assets/css/shared/iconly.css">
